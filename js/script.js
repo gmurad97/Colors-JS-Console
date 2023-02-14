4b26c41cf9628eeb2b9957762c2b4e6e5affff00
gmurad97 <murad.dev@bk.ru>
2023-02-14 06:32:48 +0400

    release

---
+++ js/script.js
@@ -0,0 +1,5 @@
+console.log("%c \u2729 SETHUB \u2729 STIMUL \u2729 ", "font-weight: bold; font-size: 64px; color: #8e44ad; text-shadow: 0px 0px 10px #9b59b6");
+console.error("%c HTML", "color: red;");
+console.error("%c CSS", "color: green;");
+console.error("%c JavaScript ", "color: blue;");
+console.log("default text");
